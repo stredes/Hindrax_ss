f696397d1c253deae607b819fb8b6294e170fa99
stredes <gianlucassanmartin@gmail.com>
2026-05-18 22:37:56 -0400

    Automated Release v1.5 (Build 6): Update node core, security optimizations and mission sync

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.hindrax.ss"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
