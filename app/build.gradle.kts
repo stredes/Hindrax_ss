443fa2d5da1c477aee129b088f878d748e808e0e
stredes <gianlucassanmartin@gmail.com>
2026-05-22 22:19:49 -0400

    release: v1.18 build 19 - Update Hindrax core and release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.hindrax.ss"
         minSdk = 24
         targetSdk = 36
-        versionCode = 18
-        versionName = "1.17"
+        versionCode = 19
+        versionName = "1.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
