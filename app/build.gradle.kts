0bf8b05bdc77eff5a42d0c44fb40e2a95db60c24
stredes <gianlucassanmartin@gmail.com>
2026-05-23 21:50:41 -0400

    release: v1.45 build 46 - Update Hindrax core and release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,8 @@ android {
         applicationId = "com.hindrax.ss"
         minSdk = 24
         targetSdk = 36
-        versionCode = 45
-        versionName = "1.44"
+        versionCode = 46
+        versionName = "1.45"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
